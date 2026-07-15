822efbdd278fb57eaadeef5221fb90df5197c8ca
unknown <leonard@hoxxes.com>
2026-07-15 14:51:01 +0200

    logo update

--- components/LogoMarquee.tsx
+++ components/LogoMarquee.tsx
@@ -3,12 +3,13 @@
 import Marquee from "react-fast-marquee";
 
 const logos = [
-  "lejza",
-  "dejavu",
-  "tomos",
-  "heavyhit",
-  "cheese",
-  "pasa",
+  { name: "lejza", file: "lejza.svg" },
+  { name: "dejavu", file: "dejavu.svg" },
+  { name: "tomos", file: "tomos.svg" },
+  { name: "heavyhit", file: "heavyhit.svg" },
+  { name: "cheese", file: "cheese.svg" },
+  { name: "pasa", file: "pasa.svg" },
+  { name: "proper", file: "proper.png" },
 ];
 
 export default function LogoMarquee() {
@@ -21,13 +22,13 @@ export default function LogoMarquee() {
     autoFill
   >
         {logos.map((logo) => (
-          <img
-            key={logo}
-            src={`https://hoxxes.app/images/${logo}.svg`}
-            alt={logo}
-            className="h-10 md:h-12 lg:h-12 w-auto object-contain mx-10 opacity-70 hover:opacity-100 transition-all duration-300"
-          />
-        ))}
+  <img
+    key={logo.name}
+    src={`https://hoxxes.app/images/${logo.file}`}
+    alt={logo.name}
+    className="h-10 md:h-12 lg:h-12 w-auto object-contain mx-10 grayscale opacity-70 hover:grayscale-0 hover:opacity-100 transition-all duration-300"
+  />
+))}
       </Marquee>
     </div>
   );
